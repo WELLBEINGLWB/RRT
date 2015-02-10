784d2c4c1c6ac93fd6bac4ced217a90ab2fa4b16
Ry0 <k104037r@mail.kyutech.jp>
2015-02-10 21:58:45 +0900

    数値取り

--- 2D/rrt5/MotionPlan.cpp
+++ 2D/rrt5/MotionPlan.cpp
@@ -634,6 +634,8 @@ void MotionPlan::RRT::RRTloop(int* iterations, int* nodePath, int* pathLength, s
   }
   #ifdef Smooth
   smoothing(10000);
+  #else
+  // cout << "パスの総数は" << paths.size() << endl;
   #endif
 }
 
